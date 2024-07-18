b52d1c9865a53954306139af5f619b25be30e8e3
Jdob11 <jbdobson11@gmail.com>
2024-07-18 13:56:50 -0700

    database: Add seeds to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+ProgrammingLanguage.create([
+  { title: 'Ruby', description: 'A dynamic, open source programming language with a focus on simplicity and productivity.', link: 'https://www.ruby-lang.org/' },
+  { title: 'Python', description: 'A powerful, high-level programming language that is easy to learn and has a wide range of applications.', link: 'https://www.python.org/' },
+  { title: 'JavaScript', description: 'A versatile, high-level programming language that is a core technology of the World Wide Web.', link: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+  { title: 'Java', description: 'A class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.', link: 'https://www.oracle.com/java/' },
+  { title: 'C++', description: 'An extension of the C programming language with object-oriented features and powerful abstractions.', link: 'https://isocpp.org/' }
+])
